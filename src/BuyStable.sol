a0e6998d8d54daf75906b45c9baa1f7046153cfb
Bayological <6872903+bayological@users.noreply.github.com>
2022-10-17 12:50:58 +0100

    buy mento stable example

--- src/BuyStable.sol
+++ src/BuyStable.sol
@@ -1,22 +1,35 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity 0.8.15;
 
+import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import { IMentoExchange } from "./interfaces/IMentoExchange.sol";
-import { IMentoStable } from "./interfaces/IMentoStable.sol";
 
 contract BuyStable {
     IMentoExchange public cUSDExchange;
-    IMentoStable public cUSD;
+    IERC20 public celoToken;
+    IERC20 public cUSDToken;
 
-    constructor(IMentoExchange _cUSDExchange, IMentoStable _cUSD) {
+    constructor(
+        IMentoExchange _cUSDExchange,
+        IERC20 _celoToken,
+        IERC20 _cUSDToken
+    ) {
         cUSDExchange = _cUSDExchange;
-        cUSD = _cUSD;
+        celoToken = _celoToken;
+        cUSDToken = _cUSDToken;
     }
 
-    function doMinting(address to, uint256 value) external {
-        // Create reference to Exchange
-        // Create reference to cUSD
-        // Approve Exchange to spend senders CELO
-        //
+    function BuyStableWithCelo(uint256 amountOfCeloToSell) external {
+        // Approve spend
+        celoToken.approve(msg.sender, amountOfCeloToSell);
+
+        // Transfer Celo to contract
+        celoToken.transferFrom(msg.sender, address(this), amountOfCeloToSell);
+
+        // Sell a known amount of CELO for cUSD
+        uint256 cUSDReceived = cUSDExchange.sell(amountOfCeloToSell, 0, true);
+
+        // Transfer cUSD to msg sender
+        cUSDToken.transfer(msg.sender, cUSDReceived);
     }
 }
--- src/interfaces/IMentoExchange.sol
+++ src/interfaces/IMentoExchange.sol
@@ -2,5 +2,9 @@
 pragma solidity 0.8.15;
 
 interface IMentoExchange {
-    
+    function sell(
+        uint256 sellAmount,
+        uint256 minBuyAmount,
+        bool sellCelo
+    ) external returns (uint256);
 }
