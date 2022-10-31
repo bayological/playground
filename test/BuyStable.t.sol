b3ba5f733f4b80ec379f9b33990b4e54f9f647b9
Bayological <6872903+bayological@users.noreply.github.com>
2022-10-31 16:20:52 +0100

    test: add fake test function (#1) (#2)
    * test: add fake test function
    
    * ci: run checks on dev
    
    * style: run lint

---
+++ test/BuyStable.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.15;
+
+contract BuyStableTest {
+    function test_someFunction_whenCalled_doesSomething() public pure returns (bool) {
+        return true;
+    }
+}
