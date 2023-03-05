17fdfb20081c14b3f15af10ea9218dde0f282b1b
Bayological <6872903+bayological@users.noreply.github.com>
2023-03-05 13:32:12 -0500

    chore: remove sample code

--- test/BuyStable.t.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicensed
-pragma solidity 0.8.15;
-
-contract BuyStableTest {
-    function test_someFunction_whenCalled_doesSomething() public pure returns (bool) {
-        return true;
-    }
-}
